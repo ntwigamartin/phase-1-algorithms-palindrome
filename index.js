330aa1f59e702d0a48b9c8600ffbd9a42e46e3c2
ntwigamartin <ntwiga.martin@gmail.com>
2022-12-17 12:25:59 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  return [...word].reverse().join("").toLowerCase()=== word.toLowerCase()
 }
-
+console.log(isPalindrome("Racecar"));
 /* 
   Add your pseudocode here
+  convert word to array, reverse array and join then compare with initial word
 */
 
 /*
   Add written explanation of your solution here
+  word needs to be an array to enable reverse
+  strict equality operator is used to compare both value and type
+  comparison evaluates to a boolean
 */
 
 // You can run `node index.js` to view these console logs
